fca66097778aa36e3dee5fe8b57ee3698ad69521
Sam Selikoff <sam.selikoff@gmail.com>
2025-12-12 11:48:53 -0500

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -3,38 +3,51 @@ import { Suspense } from 'react';
 
 export default function Home() {
   return (
-    <div>
-      <CachedComponent />
-      <Suspense fallback="Loading...">
-        <DynamicComponent />
-      </Suspense>
-      <form
-        action={async () => {
-          'use server';
-          revalidatePath('/');
-        }}
-      >
-        {/* ✅ Refreshes CachedComponent and DynamicComponent */}
-        <button>revalidatePath</button>
-      </form>
-      <form
-        action={async () => {
-          'use server';
-          console.log('refresh');
-          refresh();
-        }}
-      >
-        {/* ✅ Locally only refreshes DynamicComponent. */}
-        {/* ❌ When deployed to Vercel refreshes both CachedComponent and DynamicComponent */}
-        <button>refresh</button>
-      </form>
+    <div className="p-4">
+      <div className="flex gap-2">
+        <div className="border p-2">
+          <p className="text-sm uppercase text-gray-500">Cached component</p>
+          <CachedComponent />
+        </div>
+
+        <div className="border p-2">
+          <p className="text-sm uppercase text-gray-500">Dynamic component</p>
+          <Suspense fallback="Loading...">
+            <DynamicComponent />
+          </Suspense>
+        </div>
+      </div>
+
+      <div className="mt-8 flex gap-4">
+        <form
+          action={async () => {
+            'use server';
+            revalidatePath('/');
+          }}
+        >
+          {/* ✅ Refreshes CachedComponent and DynamicComponent */}
+          <button className="bg-black text-white px-2 py-1">
+            revalidatePath
+          </button>
+        </form>
+        <form
+          action={async () => {
+            'use server';
+            console.log('refresh');
+            refresh();
+          }}
+        >
+          {/* ✅ Locally only refreshes DynamicComponent. */}
+          {/* ❌ When deployed to Vercel refreshes both CachedComponent and DynamicComponent */}
+          <button className="bg-black text-white px-2 py-1">refresh</button>
+        </form>
+      </div>
     </div>
   );
 }
 
 async function CachedComponent() {
   'use cache';
-  // cacheLife("days");
   const data = await fetchRandomNumber();
   return <div>{data}</div>;
 }
